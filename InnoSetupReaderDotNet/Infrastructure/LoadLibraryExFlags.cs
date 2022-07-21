c5bf30236480b944c76bd200717748c5da763d0d
Todd Aspeotis <t@speot.is>
2022-07-21 20:05:39 +1000

    wip

---
+++ InnoSetupReaderDotNet/Infrastructure/LoadLibraryExFlags.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace InnoSetupReaderDotNet.Infrastructure;
+
+/// <see href="https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibraryexw" />
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+internal enum LoadLibraryExFlags
+{
+    LOAD_LIBRARY_AS_DATAFILE = 0x00000002
+}
---
+++ InnoSetupReaderDotNet/Infrastructure/NativeMethods.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace InnoSetupReaderDotNet.Infrastructure;
+
+internal static class NativeMethods
+{
+    private const string Kernel32 = "kernel32.dll";
+
+    /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibraryexw" />
+    [DllImport(Kernel32, SetLastError = true)]
+    public static extern IntPtr LoadLibraryEx(string fileName, IntPtr _, LoadLibraryExFlags flags);
+
+    /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-freelibrary" />
+    [DllImport(Kernel32, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool FreeLibrary(IntPtr module);
+}
